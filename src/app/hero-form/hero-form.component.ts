4c99a61911483e85105ee7a2e833a2cf698208a9
devliet <101347493+devliet@users.noreply.github.com>
2023-03-22 13:02:12 +0100

    3 files modified, 1 added, 1 deleted

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -2,18 +2,18 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
-  selector: app-hero-form;
+  selector: app-hero-form,
   templateUrl: './hero-form.component.html',
   styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent{
-  powers = ['Really Smart', 'Super Flexible',
-  'Super Hot', 'Weather Changer'];
-  model= new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
-  submitted = false;
-  onSubmit() = {this.submitted = true;}
-  const myHero = new Hero(42, 'SkyDog',
-  'Fetch any object at any distance',
-  'Leslie Rollover');
-  console.log('Hero is called ' + myHero.name);
+    powers = ['Really Smart', 'Super Flexible',
+    'Super Hot', 'Weather Changer'];
+    model= new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+    submitted = false;
+    onSubmit() {
+      this.submitted = true;
+    }
+    const myHero = new Hero(42, 'SkyDog', 'Fetch any object at any distance', 'Leslie Rollover');
+   // console.log('Hero is called ' + myHero.name);
 }
